282e01f05d9338e017e8b7382b2008acfbac5707
ssanchis <128268824+ssanchis@users.noreply.github.com>
2025-04-29 20:32:48 +0200

    modificando alertas programacion

--- alertas_cron.py
+++ alertas_cron.py
@@ -8,7 +8,9 @@ def main():
     while True:
         print(f"🚀 Ejecutando script de alertas... {datetime.datetime.now()}")
         datos_hist, datos_recientes = descargar_datos()
-        check_alertas(datos_hist, datos_recientes)
+        alertas = check_alertas(datos_hist, datos_recientes)
+        if alertas:
+            print('Find', len(alertas),'alertas')
         print("✅ Esperando 1 hora para la siguiente ejecución...\n")
         time.sleep(3600)  # 3600 segundos = 1 hora
 
