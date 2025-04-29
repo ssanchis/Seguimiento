1e69fb07481059d411127925b865f4f33d17f030
ssanchis <128268824+ssanchis@users.noreply.github.com>
2025-04-29 18:37:44 +0200

    cosas de alertas

--- alertas_cron.py
+++ alertas_cron.py
@@ -1,11 +1,16 @@
 # alertas_cron.py
 
 from alertas_core import descargar_datos, check_alertas
+import time
+import datetime
 
 def main():
-    print("🚀 Ejecutando script de alertas...")
-    datos_hist, datos_recientes = descargar_datos()
-    check_alertas(datos_hist, datos_recientes)
+    while True:
+        print(f"🚀 Ejecutando script de alertas... {datetime.datetime.now()}")
+        datos_hist, datos_recientes = descargar_datos()
+        check_alertas(datos_hist, datos_recientes)
+        print("✅ Esperando 1 hora para la siguiente ejecución...\n")
+        time.sleep(3600)  # 3600 segundos = 1 hora
 
 if __name__ == "__main__":
     main()
